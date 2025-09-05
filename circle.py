04680fe127cc4e0de91224a196836c2a366c2c30
Ros_Nikki <roshinikitha@gmail.com>
2025-09-05 13:52:34 -0500

    commit

---
+++ circle.py
@@ -0,0 +1,18 @@
+"""
+Week 1: Lecture 2 - Intro to python 
+
+    Write a python function called circle_area, t
+    hat calculate the area of a circle given a radius parameter 
+    (it returns the circle area).
+
+-> area = pi * radius^2 
+-> use 3.1415 for 𝜋
+
+"""
+
+import math
+
+
+def circle_area(radius): 
+    return (3.1415 * (radius **2)) 
+
